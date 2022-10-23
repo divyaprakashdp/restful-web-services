586a21fe5b7d584f21bdf287b7668e6102dca37e
divyaprakash <d.prakash1506@gmail.com>
2022-10-23 16:43:00 +0530

    added todoService

--- src/main/java/com/sprinfgDemo/rest/webServices/helloworld/HelloWorldController.java
+++ src/main/java/com/sprinfgDemo/rest/webServices/helloworld/HelloWorldController.java
@@ -4,20 +4,22 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController {
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
-        return "Hello World";
+//        return "Hello World";
+        throw new RuntimeException("Something went wrong");
     }
 
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hello World");
+        return new HelloWorldBean("Hello World!!!");
     }
 
-    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
-    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
-        return new HelloWorldBean(String.format("Hello World, %s", name));
+    @GetMapping(path = "/hello-world/path-variable/{user}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String user){
+        return new HelloWorldBean(String.format("Hello World-->, %s", user));
     }
 }
